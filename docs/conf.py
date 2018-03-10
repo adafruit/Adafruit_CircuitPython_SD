0c91832315c1629cd45ca45859128e8c4613cedf
sommersoft <sommersoft@gmail.com>
2018-03-10 11:27:41 -0600

    updated conf.py; bus device intersphinx link

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ extensions = [
 autodoc_mock_imports = ["adafruit_bus_device", "micropython"]
 
 intersphinx_mapping = {'python': ('https://docs.python.org/3.4', None),
-                       'BusDevice': ('https://circuitpython.readthedocs.io/projects/bus_device/en/latest/', None),
+                       'BusDevice': ('https://circuitpython.readthedocs.io/projects/busdevice/en/latest/', None),
                        'CircuitPython': ('https://circuitpython.readthedocs.io/en/latest/', None)}
 
 # Add any paths that contain templates here, relative to this directory.
